a975d7da32b059b604aa422bf9e1fed6d0fab980
alou <01008@protonmail.com>
2022-03-19 20:44:38 -0700

    add create_poll function

--- models/Poll.py
+++ models/Poll.py
@@ -11,31 +11,37 @@ class Poll(db.Model):
   user_id: str
   category_id: int
   name: str
+  center: str
+  restriction: str
   description: str
   created_at: datetime
   start_at: datetime
   end_at: datetime
+  visibility: str
 
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.String, db.ForeignKey('public.user.id'), nullable=False)
   category_id = db.Column(db.Integer, db.ForeignKey('public.category.id'), nullable=False)
   name = db.Column(db.String, nullable=False)
-  # center = db.Column(db.String)
-  # restriction = db.Column(db.String)
+  center = db.Column(db.String)
+  restriction = db.Column(db.String)
   description = db.Column(db.Text)
   created_at = db.Column(db.DateTime, nullable=False)
   start_at = db.Column(db.DateTime, nullable=False)
   end_at = db.Column(db.DateTime, nullable=False)
-  # visibility = db.Column(db.Numeric)
+  visibility = db.Column(db.String)
 
   # list of answers for poll
   answers = db.relationship('Answer', cascade='all, delete')
 
-  def __init__(self, user_id, category_id, name, description, start_at, end_at):
+  def __init__(self, user_id, category_id, name, center, restriction, description, start_at, end_at, visibility):
     self.user_id = user_id
     self.category_id = category_id
     self.name = name
+    self.center = center
+    self.restriction = restriction
     self.description = description
     self.created_at = datetime.now()
     self.start_at = start_at
     self.end_at = end_at
+    self.visibility = visibility
