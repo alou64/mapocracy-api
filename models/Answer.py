2cb20dc3136cdfbdeddab16462681411bf809638
alou64 <81368883+alou64@users.noreply.github.com>
2022-03-21 11:52:33 -0700

    Flask restless (#3)
    * refactor models
    
    * refactor models
    
    * cache

--- models/Answer.py
+++ models/Answer.py
@@ -1,14 +1,15 @@
+# from flask_sqlalchemy import SQLAlchemy
 from database import db
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
-@dataclass
+# @dataclass
 class Answer(db.Model):
   __tablename__ = 'answer'
   __table_args__ = {'schema': 'public'}
 
-  id: int
-  poll_id: str
-  content: str
+  # id: int
+  # poll_id: str
+  # content: str
 
   id = db.Column(db.Integer, primary_key=True)
   poll_id = db.Column(db.Integer, db.ForeignKey('public.poll.id'), nullable=False)
@@ -17,6 +18,6 @@ class Answer(db.Model):
   # list of votes for answer
   votes = db.relationship('Vote', cascade='all, delete')
 
-  def __init__(self, poll_id, content):
-    self.poll_id = poll_id
-    self.content = content
+  # def __init__(self, poll_id, content):
+  #   self.poll_id = poll_id
+  #   self.content = content
--- models/Vote.py
+++ models/Vote.py
@@ -1,24 +1,24 @@
 from database import db
-from dataclasses import dataclass
+# from dataclasses import dataclass
 from datetime import datetime
 
 # many to many association object
-@dataclass
+# @dataclass
 class Vote(db.Model):
   __tablename__ = 'vote'
   __table_args__ = {'schema': 'public'}
 
-  user_id: str
-  poll_id: int
-  answer_id: int
-  voted_at: datetime
+  # user_id: str
+  # poll_id: int
+  # answer_id: int
+  # voted_at: datetime
 
-  user_id = db.Column(db.String, db.ForeignKey('public.user.id'), primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('public.user.id'), primary_key=True)
   poll_id = db.Column(db.Integer, db.ForeignKey('public.poll.id'), primary_key=True)
   answer_id = db.Column(db.Integer, db.ForeignKey('public.answer.id'), nullable=False)
   voted_at = db.Column(db.DateTime, nullable=False)
 
-  def __init__(self, user_id, poll_id, answer_id):
-    self.user_id = user_id
-    self.poll_id = poll_id
-    self.answer_id = dateime.now()
+  # def __init__(self, user_id, poll_id, answer_id):
+  #   self.user_id = user_id
+  #   self.poll_id = poll_id
+  #   self.answer_id = dateime.now()
