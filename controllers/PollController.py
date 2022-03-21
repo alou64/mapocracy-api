7d884e093c8e63fb48c5dc2c46f634d61663cede
alou64 <81368883+alou64@users.noreply.github.com>
2022-03-20 18:04:20 -0700

    add more routes for user, category, poll, answer (#1)
    * add create_user function
    
    * change user primary key to email
    
    * add create_poll function
    
    * add get_user_poll function

--- controllers/PollController.py
+++ controllers/PollController.py
@@ -1,7 +1,34 @@
 from models.Poll import Poll
+from models.Answer import Answer
 from flask import Flask, request, jsonify, make_response
 from database import db
+from datetime import datetime
 
 
 def index():
-  pass
+  q = Poll.query.all()
+  return jsonify(q)
+
+def create_poll():
+  req = request.json
+  poll = Poll(
+    req['user_id'],
+    int(req['category_id']),
+    req['name'],
+    req['center'],
+    req['restriction'],
+    req['description'],
+    datetime.fromtimestamp(int(req['start_at'])),
+    datetime.fromtimestamp(int(req['end_at'])),
+    req['visibility']
+  )
+  db.session.add(poll)
+  db.session.flush()
+  for item in req['answers']:
+    answer = Answer(poll.id, item)
+    db.session.add(answer)
+  db.session.commit()
+  return jsonify(poll)
+
+
+# datetime.fromtimestamp(int('1364694508'))
