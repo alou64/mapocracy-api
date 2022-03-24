ed580f336f13b448d6fc5fede47e81eb2d2e1c4f
alou64 <81368883+alou64@users.noreply.github.com>
2022-03-23 17:20:51 -0700

    Refactor routes (#9)
    * implement dynamic poll filtering
    
    * add answers to polls returned by user query

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -31,61 +31,57 @@ def create_user():
 
 
 def get_user_poll(user_id):
-  if request.args['time'] == 'current':
-    return jsonify(
-      Poll.query
-        .filter(Poll.user_id == user_id, Poll.end_at >= datetime.now())
-        .all()
-    )
+  if not request.args:
+    return jsonify([
+      [poll, poll.answers]
+        for poll in Poll.query
+          .all()
+    ])
 
-  return jsonify(
-    Poll.query
-    .filter(Poll.user_id == user_id, Poll.end_at < datetime.now())
-    .all()
-  )
+  if request.args['time'] == 'current':
+    return jsonify([
+      [poll, poll.answers]
+        for poll in Poll.query
+          .filter(Poll.user_id == user_id, Poll.end_at >= datetime.now())
+          .all()
+    ])
+
+  return jsonify([
+    [poll, poll.answers]
+      for poll in Poll.query
+        .filter(Poll.user_id == user_id, Poll.end_at < datetime.now())
+        .all()
+  ])
 
 
 def get_user_invites(user_id):
-  return jsonify(
-    Poll.query
-      .join(VoterList, Poll.restriction == VoterList.id)
-      .join(VoterListMember, VoterList.id == VoterListMember.voter_list_id)
-      .filter(VoterListMember.user_id == user_id)
-      .all()
-  )
+  return jsonify([
+    [poll, poll.answers]
+      for poll in Poll.query
+        .join(VoterList, Poll.restriction == VoterList.id)
+        .join(VoterListMember, VoterList.id == VoterListMember.voter_list_id)
+        .filter(VoterListMember.user_id == user_id)
+        .all()
+  ])
 
 
 def get_user_voter_list(user_id):
-  return jsonify(
-    {
-      VoterList.id:
-        [
-          VoterListMember.user_id for VoterListMember in VoterList.voter_list_members
-            ]
-              for VoterList in VoterList.query
-                .filter(VoterList.user_id == user_id)
-                .all()
-      }
-  )
+  return jsonify({
+    VoterList.id:
+      [VoterListMember.user_id
+        for VoterListMember in VoterList.voter_list_members]
+          for VoterList in VoterList.query
+            .filter(VoterList.user_id == user_id)
+            .all()
+  })
 
 
 def update_user():
   req = request.json
   user = User.query.get(req['id'])
 
-  user.first_name = req['first_name']
-  user.last_name = req['last_name']
-  user.longtitude = req['longtitude']
-  user.latitude = req['latitude']
-  user.age = req['age']
-  user.gender = req['gender']
-  user.ethnicity = req['ethnicity']
-  user.industry = req['industry']
-  user.religion = req['religion']
-  user.income_range = req['income_range']
-  user.education = req['education']
-  user.marital_status = req['marital_status']
-  user.veteran = req['veteran']
+  for key, val in req.items():
+    user.key = val
 
   db.session.commit()
 
--- routes/category_bp.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers.CategoryController import index, create_category
-
-category_bp = Blueprint('category_bp', __name__)
-
-category_bp.route('/', methods=['GET'])(index)
-category_bp.route('/new', methods=['POST'])(create_category)
