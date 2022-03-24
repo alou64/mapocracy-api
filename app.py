07e7192f2948a0c3dc707d562f6a78e1574aa32c
alou64 <81368883+alou64@users.noreply.github.com>
2022-03-24 07:16:43 -0700

    add flask-CORS extension (#10)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from database import db
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from routes.answer_bp import answer_bp
 from routes.poll_bp import poll_bp
@@ -9,16 +10,14 @@ from routes.vote_bp import vote_bp
 from routes.voter_list_bp import voter_list_bp
 from routes.voter_list_member_bp import voter_list_member_bp
 
-# from populate_db import populate_db
-
 app = Flask(__name__)
 app.config.from_object('config')
 app.url_map.strict_slashes = False
 
+cors = CORS(app, origins='http://localhost:3000')
+
 db.init_app(app)
 migrate = Migrate(app, db)
-
-# populate_db()
 
 app.register_blueprint(answer_bp, url_prefix='/answer')
 app.register_blueprint(poll_bp, url_prefix='/poll')
