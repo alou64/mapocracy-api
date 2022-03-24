da4abb9ecc76c89d33832e0bd3d2f8129b8f018d
alou64 <81368883+alou64@users.noreply.github.com>
2022-03-24 15:21:25 -0700

    add vote count for answers and add voter location to poll query (#11)

--- routes/poll_bp.py
+++ routes/poll_bp.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from controllers.PollController import create_poll, get_poll_by_id, filter_polls
+from controllers.PollController import create_poll, get_poll_by_id, filter_polls, test
 
 poll_bp = Blueprint('poll_bp', __name__)
 
 poll_bp.route('/', methods=['GET'])(filter_polls)
+poll_bp.route('/test', methods=['GET'])(test)
 poll_bp.route('/<int:id>', methods=['GET'])(get_poll_by_id)
 poll_bp.route('/new', methods=['POST'])(create_poll)
